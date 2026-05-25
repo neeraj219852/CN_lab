90b3282182373bfbb7ca4bdb0a3e4e516e7048ed
Neeraj Vinod <neerajvinod11@gmail.com>
2026-05-25 16:31:43 +0530

    Update server.c

--- stopnwait/server.c
+++ stopnwait/server.c
@@ -12,36 +12,36 @@ int main()
     printf("\nWaiting for client...\n");
 
     // Structure to store server and client address
-    struct sockaddr_in servaddr, cliaddr;
+    struct sockaddr_in server, client;
 
     // Create UDP socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    int  s = socket(AF_INET, SOCK_DGRAM, 0);
 
     // Clear memory of structures
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
 
     // Set server address details
-    servaddr.sin_family = AF_INET;        // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY; // Accept connection from any IP
-    servaddr.sin_port = htons(8080);       // Port number
+    server.sin_family = AF_INET;        // IPv4
+    server.sin_addr.s_addr = INADDR_ANY; // Accept connection from any IP
+    server.sin_port = htons(8080);       // Port number
 
     // Bind socket with server address
-    bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    bind( s, (const struct sockaddr *)&server, sizeof(server));
 
-    int len = sizeof(cliaddr);
+    int len = sizeof(client);
 
     int frames[100], n;
 
     // Receive total number of frames from client
-    recvfrom(sockfd, &n, sizeof(n), 0, (struct sockaddr *)&cliaddr, &len);
+    recvfrom( s, &n, sizeof(n), 0, (struct sockaddr *)&client, &len);
 
     int p, ack;
 
     while (1)
     {
         // Receive frame number
-        recvfrom(sockfd, &p, sizeof(n), 0, (struct sockaddr *)&cliaddr, &len);
+        recvfrom( s, &p, sizeof(n), 0, (struct sockaddr *)&client, &len);
 
         // If client sends -99 transmission ends
         if (p == -99)
@@ -55,7 +55,7 @@ int main()
         scanf("%d", &ack);
 
         // Send ACK back to client
-        sendto(sockfd, &ack, sizeof(n), 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+        sendto( s, &ack, sizeof(n), 0, (struct sockaddr *)&client, sizeof(client));
     }
 
     return 0;
